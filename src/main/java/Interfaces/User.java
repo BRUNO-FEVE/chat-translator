22f1f0deeaf707afe90c355376c1a30bded8c17f
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-11-12 23:12:23 -0300

    fix: add `email` user prop

--- src/main/java/Interfaces/User.java
+++ src/main/java/Interfaces/User.java
@@ -2,15 +2,22 @@ package Interfaces;
 
 public class User {
     public String name;
+    public String email;
+    public String password;
     public String phoneNumber;
     public String language;
-    public String picturePath;
 
-    public User(String name, String phoneNumber, String language, String picture) {
+    public User(String name, String email, String password, String phoneNumber, String language) {
         this.name = name;
+        this.email = email;
+        this.password = password;
         this.phoneNumber = phoneNumber;
         this.language = language;
-        this.picturePath = picture;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
 }
